14654f67c93d7c20c50e587850864d829a9b35d0
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-12-06 16:11:05 +0100

    Update configBD.phpDatos BD

--- JaviParte/config/configBD.php
+++ JaviParte/config/configBD.php
@@ -3,8 +3,4 @@
     define("USUARIO",'root');
     define("PASSWORD",'');
     define("BBDD",'preguntadaw');
-    //controlador por defecto
-    define("CONDEF",'PreguntasRespuestas');
-    //metodo por defecto 
-    define("METDEF",'mostrarCreacion');
 ?>
