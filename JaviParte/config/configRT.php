c397bc835b4ef30c45e97447366907c91345df7e
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-12-08 12:22:18 +0100

    antigua parte

--- JaviParte/config/configRT.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //controlador por defecto
-    define("CONDEF",'PreguntasRespuestas');
-    //metodo por defecto 
-    define("METDEF",'mostrarCreacion');
-//ruta relativa de las imagenes
-    define('RUTA_IMAGENES_PREGUNTAS', './vistas/img/preguntasImagenes/');
-?>
