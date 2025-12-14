f861b5e9822a1042be5a628513f33b4e1a8b1116
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-12-14 18:09:55 +0100

    ...

--- dwes_hosting_jc(si borra instalacion)/gestionadmin/config/configdb.php
+++ dwes_hosting_jc(si borra instalacion)/gestionadmin/config/configdb.php
@@ -1,10 +1,3 @@
 <?php
-	define ('SERVIDOR', 'daw.esvirgua.com');
-	define ('USUARIO', 'daw_userbd24');
-	define ('PASSWORD', 'BbXC45=73VD9Z-(S');
-	define ('BBDD', 'daw_24_BD1');
 
-	/* Default options */
-	define("DEFAULT_CONTROLLER", "controladorIndex");
-	define("DEFAULT_ACTION", "iniciarVista");
 ?>
